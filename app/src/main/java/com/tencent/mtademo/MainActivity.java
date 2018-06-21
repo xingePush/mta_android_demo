57b67f7525bcb7bc04e367505cfed31f31426751
foreachli(李国栋) <foreachli@tencent.com>
2018-06-21 17:50:08 +0800

    init commit

---
+++ app/src/main/java/com/tencent/mtademo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.tencent.mtademo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tencent.stat.StatConfig;
+import com.tencent.stat.StatService;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
